1a4394fe84df2e89d724b7c9b5f16530b061eaa2
Forest Wang <forest_wanglin@163.com>
2023-11-07 13:57:28 +0800

    D

--- jtokkit/src/main/java/xyz/felh/openai/jtokkit/AbstractEncodingRegistry.java
+++ jtokkit/src/main/java/xyz/felh/openai/jtokkit/AbstractEncodingRegistry.java
@@ -42,6 +42,9 @@ abstract class AbstractEncodingRegistry implements EncodingRegistry {
             return Optional.of(getEncodingForModel(ModelType.GPT_3_5_TURBO_16K));
         }
 
+        if (modelName.startsWith(ModelType.GPT_3_5_TURBO_1106.getName())) {
+            return Optional.of(getEncodingForModel(ModelType.GPT_3_5_TURBO_1106));
+        }
         if (modelName.startsWith(ModelType.GPT_3_5_TURBO.getName())) {
             return Optional.of(getEncodingForModel(ModelType.GPT_3_5_TURBO));
         }
